68bfc0176e306d2667d6b5475a5368b2a411930f
PedroVyttor <vyttinho18@gmail.com>
2025-10-24 20:36:22 -0300

    anotacao

--- petshop.py
+++ petshop.py
@@ -1,2 +1,38 @@
+#---------Menu (USUARIO)-----------
+#1- Cadastro
+#2- Login
+#0- Sair
+#
+#(Cadastro (Usuario))
+#Nome do usuario:
+#Email:
+#Senha:
+#
+#
+#(Tela inicial(Usuario))
+#1- Buscar Produto
+#           Lista de Produtos
+#           1- Caldo de cana                      x1   R$10,00
+#           2- Bola de futebol americano          x9   R$0,05
+#           3- Alicate de unha                    x384   R$156,00
+#
+#           Selecionar produto: x
+#           0- Voltar
+#
+#                  Digite a quantidade: 1
+#                          Confirmar: Sim
+#                                 se "não"
+#                                                 Selecionar produto: x ...
+#                                                 0- Voltar
+#                          1- Caldo de cana                                x0   R$10,00
+#                          2- Bola de futebol americano          x9   R$0,05
+#                          3- Alicate de unha                             x384   R$156,00
+#           Quer fazer uma lista:
+#                    whlle
+#                           qual item dejesa adicionar a lista: 1 32 3 7 68
+#                           voltar
+#                                  adicionar outro item :
+#                                   terminar lista
+
 print('------------MENU (USUARIO)----------\n 1- Lista\n {x}- Numero\n 3- Fulano\n 0- Sair')
 
