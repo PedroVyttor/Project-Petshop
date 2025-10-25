757f71aac250eb1bf50250ed1287c01518a963fe
PedroVyttor <vyttinho18@gmail.com>
2025-10-25 15:36:35 -0300

    menu admin

--- petshop.py
+++ petshop.py
@@ -1,14 +1,14 @@
+#       ---- Projeto Petshop ----
+#   Algoritmos e Lógica de Programação
+#   Dupla: Pedro Vyttor, Marcus Vinicius (Periodo 1)
+#   Linguagem: Python
+
+
+
 #---------Menu (USUARIO)-----------
 #1- Cadastro
 #2- Login
 #0- Sair
-#
-
-
-
-
-
-
 
 # 1 - (Cadastro (Usuario))
 #Nome do usuario:
@@ -17,7 +17,10 @@
 #senha nao pode ter menos de 4 digitos
 #.lower()
 #tem q ter (@) (gmail) e (.com)
-#email n pode ser igual a um email ja cadastrado
+#email nao pode ser igual a um email ja cadastrado
+
+
+
 #(Tela inicial(Usuario))
 #1- Buscar Produto
 #           Lista de Produtos
@@ -49,10 +52,10 @@
 #                                                                               colcoar o usuario correspondente a da lista.
 #
 #                                                                               1 - AGENDAR SERVIÇOS
-#                                                                                     1-tosa
+#                                                                                     1-tosa (explicca o serviço)
 #                                                                                     2-banho
 #                                                                                     3-exame
-#                                                                                     4-adoçao
+#                                                                                     4-adoçao (explicca o processo)
 #                                                                               2 - COMPRAR PRODUTOS
 #                                                                                    ----------- PRODUTOS ------------
 #                                                                                       1 - BLA BLA BLA
@@ -64,26 +67,25 @@
 #                                                                                            se haver simbolos (: ; , .), desconsiderar
 #
 #                                                                                                       Ao selecionar, exibir a mensagem: "Confirmar?"
-#                                                                                                             ao confirmar (SIM): print da lista dos pedidos que selecionou
+#                                                                                                             ao confirmar (SIM): print da lista dos pedidos que selecionou e preço dos produtos e valor total
 #
 #                                                                                                       (NÃO):
 #                                                                                                       1 - BLA BLA BLA
-# #                                                                                                     2 - chevrolet corsa
-# #                                                                                                     3 - Toyota Hilux
-# #
-# #                                                                                                     Selecione o(s) produto(s):
+#                                                                                                      2 - chevrolet corsa
+#                                                                                                     3 - Toyota Hilux
+#
+#                                                                                                      Selecione o(s) produto(s):
 #                                                                                                                            ".pop"
-#                                                                                                        (ALGO ALEATORIO)
+#                                                                                                        (ALGO ALEATORIO) ex:1223 ou aslsajdas
 #                                                                                                          "comando desconhecido"
-#                                                                                                              exibir a mensagem: "Confirmar?"
+#                                                                                                              exibir a mensagem: "Confirm
 #
-#
-#
-#
-#
-#
-#
-#
+#                                                                                                               DEVOLUÇAO (OPICIONAL)
+#                                                                                                               ADM RESOLVE
+
+
+
+
 
 
 
@@ -97,6 +99,53 @@
 
 
 # #---------Menu (ADMINISTRATIVO)-----------
+# 1 - Cadastro (admin)
+# 2 - Login (admin)
+# 0 - Sair
 
-print('------------MENU (USUARIO)----------\n 1- Lista\n {x}- Numero\n 3- Fulano\n 0- Sair')
+#CADASTRO
+# 1 - nome:
+# 2 - email:
+# 3 - senha:
+# 4 - "key": 4 digitos
+
+#LOGIN
+# 1 - email:
+# 2 - senha:
+# 3 - "key": 4 digitos
+#
+#           ---Tela inicial (Admin)---
+            #1 - Gerenciar admins
+            #2 - gerenciar usuarios
+            #3 - controle de produtos
+            #4 - servicos
+            #0 - sair
+                #1 - Gerenciar Administradores
+                        #- cadastrar admin (nome, email, senha, chave de 4 digitos)
+                        #- listar administradores
+                        #- excluir administradores
+
+
+                #2 - Gerenciar Usuários
+                        #- Listar todos os usuarios cadastrados
+                        #- Cadastrar usuario (adm ou cliente)
+                        #- buscar usuario por nome ou email
+                        #- bloquear usuario
+                        #- visualizar historico de compras do usuario (opcional)
+
+                #3 - Controle de Produtos
+                        #- Adicinoar produto (nome, descrição, quantidade, valor)
+                        #- Editar produto (alterar nome, preço ou estoque)
+                        #- Remover produto
+                        #- Listar todos os produtos (mostrar em lista, nome, quantidade e preço)
+                        #- Buscar produto (por nome, preço sla)
+
+                #4 - Serviços
+                        #- Cadastrar serviço (nome, descriçao e preco)
+                        #- Editar serviço
+                        #- excluir servico
+                        #- listar todos os servicos
+                        #- visualizar agendamentos de servicos (cliente, data, tipo de servico sla)
+
+                #0 - Sair
 
