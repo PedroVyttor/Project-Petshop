1d4b47a5d11fa28994001d17941c6ca8028017b9
PedroVyttor <vyttinho18@gmail.com>
2025-10-24 21:04:42 -0300

    anotacao

--- petshop.py
+++ petshop.py
@@ -3,12 +3,21 @@
 #2- Login
 #0- Sair
 #
-#(Cadastro (Usuario))
+
+
+
+
+
+
+
+# 1 - (Cadastro (Usuario))
 #Nome do usuario:
 #Email:
 #Senha:
-#
-#
+#senha nao pode ter menos de 4 digitos
+#.lower()
+#tem q ter (@) (gmail) e (.com)
+#email n pode ser igual a um email ja cadastrado
 #(Tela inicial(Usuario))
 #1- Buscar Produto
 #           Lista de Produtos
@@ -25,14 +34,69 @@
 #                                                 Selecionar produto: x ...
 #                                                 0- Voltar
 #                          1- Caldo de cana                                x0   R$10,00
-#                          2- Bola de futebol americano          x9   R$0,05
-#                          3- Alicate de unha                             x384   R$156,00
+#                          2- Bola de futebol americano                    x9   R$0,05
+#                          3- Alicate de unha                              x384   R$156,00
 #           Quer fazer uma lista:
 #                    whlle
 #                           qual item dejesa adicionar a lista: 1 32 3 7 68
 #                           voltar
 #                                  adicionar outro item :
 #                                   terminar lista
+
+
+#                                                                       2 - Login
+#                                                                               U / S
+#                                                                               colcoar o usuario correspondente a da lista.
+#
+#                                                                               1 - AGENDAR SERVIÇOS
+#                                                                                     1-tosa
+#                                                                                     2-banho
+#                                                                                     3-exame
+#                                                                                     4-adoçao
+#                                                                               2 - COMPRAR PRODUTOS
+#                                                                                    ----------- PRODUTOS ------------
+#                                                                                       1 - BLA BLA BLA
+#                                                                                       2 - chevrolet corsa
+#                                                                                       3 - Toyota Hilux
+#
+#                                                                                       Selecione o(s) produto(s): 2 3
+#                                                                                            menu .extend
+#                                                                                            se haver simbolos (: ; , .), desconsiderar
+#
+#                                                                                                       Ao selecionar, exibir a mensagem: "Confirmar?"
+#                                                                                                             ao confirmar (SIM): print da lista dos pedidos que selecionou
+#
+#                                                                                                       (NÃO):
+#                                                                                                       1 - BLA BLA BLA
+# #                                                                                                     2 - chevrolet corsa
+# #                                                                                                     3 - Toyota Hilux
+# #
+# #                                                                                                     Selecione o(s) produto(s):
+#                                                                                                                            ".pop"
+#                                                                                                        (ALGO ALEATORIO)
+#                                                                                                          "comando desconhecido"
+#                                                                                                              exibir a mensagem: "Confirmar?"
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+# #---------Menu (ADMINISTRATIVO)-----------
 
 print('------------MENU (USUARIO)----------\n 1- Lista\n {x}- Numero\n 3- Fulano\n 0- Sair')
 
