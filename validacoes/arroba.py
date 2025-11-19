87ce2f88c623665cac77e7f20bcc12ab34f81cd0
PedroVyttor <vyttinho18@gmail.com>
2025-11-18 21:20:59 -0300

    verificar a quantidade de '@'

---
+++ validacoes/arroba.py
@@ -0,0 +1,11 @@
+import petshop
+
+
+def repeticao(email):
+    if '@' in len(1) not in email:
+
+        #if '@' > 1:
+            #email = input('digite o email novamente')
+
+        #else:
+            #print('email valido')
